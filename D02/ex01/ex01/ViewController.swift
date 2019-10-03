866a4ba2c1d0760a1c90945438840df969232262
Rene Ramirez <rramirez@e1z1r8p21.42.us.org>
2019-10-03 12:37:28 -0700

    added ex01

--- D02/ex01/ex01/ViewController.swift
+++ D02/ex01/ex01/ViewController.swift
@@ -10,9 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 1500
+    var timer = Timer()
+    var isPlaying = false
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startLabel: UIButton!
+    @IBOutlet weak var pauseLabel: UIButton!
+    @IBOutlet weak var resetLabel: UIButton!
+    
+    @IBAction func startTimer(_ sender: AnyObject) {
+        if(isPlaying){return}
+        startLabel.isEnabled = false
+        pauseLabel.isEnabled = true
+        pauseLabel.setTitle("Pause", for: .normal)
+        startLabel.backgroundColor = UIColor.green
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        isPlaying = true
+    }
+    @IBAction func pauseTimer(_ sender: AnyObject) {
+        startLabel.isEnabled = true
+        pauseLabel.isEnabled = true
+        startLabel.backgroundColor = UIColor.white
+        
+        if(isPlaying){
+            pauseLabel.setTitle("Resume", for: .normal)
+            timer.invalidate()
+            isPlaying = false
+        }else{
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            pauseLabel.setTitle("Pause", for: .normal)
+            isPlaying = true
+        }
+    }
+    @IBAction func resetTimer(_ sender: AnyObject) {
+        startLabel.isEnabled = true
+        pauseLabel.isEnabled = false
+        pauseLabel.setTitle("Pause", for: .normal)
+        
+        timer.invalidate()
+        isPlaying = false
+        counter = 1500
+        timeLabel.text = "25:00"
+    }
+    @objc func updateTimer(){
+        counter = counter-1
+        let minutes = Int(counter) / 60
+        let seconds = Int(counter) % 60
+        
+//        let time = String(format: "%02d:%02d", minutes, seconds)
+//        print(time)
+        
+        timeLabel.text = String(format: "%02d:%02d", minutes, seconds)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timeLabel.text = "25:00"
+        pauseLabel.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
