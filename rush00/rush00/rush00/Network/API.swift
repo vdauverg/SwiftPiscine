c7f1708d60f344ffae626ae37db55ce29d057b5b
Vincent Dauvergne <vdauverg@e1z1r8p21.42.us.org>
2019-10-07 18:34:19 -0700

    le.

--- rush00/rush00/rush00/Network/API.swift
+++ rush00/rush00/rush00/Network/API.swift
@@ -1,48 +1,48 @@
-struct Movie: Decodable {
-    let id: Int!
-    let posterPath: String
-    var trailerPath: String?
-    let backdrop: String
-    let title: String
-    var releaseDate: Stringvar rating: Double
-    let overview: String
-    
-    private enum CodingKey: String, CodingKey {
-        case
-        id,
-        posterpath = "poster_path",
-        videoPath,
-        backdrop = "backdrop_path",
-        title,
-        releaseDate = "release_date",
-        rating = "vote_average",
-        overview
-    }
-}
-
-struct APIResults: Decodable {
-    let page: Int
-    let numResults: Int
-    let numPages: Int
-    let movies: [Movie]
-    
-    private enum CodingKeys: String, CodingKey {
-        case
-            page,
-            numResults = "total_results",
-            numPages = "total_pages",
-            movies = "results"
-    }
-}
-
-struct VideoResults: Decodable {
-    let details: [VideoKey]
-    
-    private enum CodingKeys: String, CodingKey {
-        case details: "results"
-    }
-}
-
-struct VideoKey: Decodable {
-    let key: String
-}
+//struct Movie: Decodable {
+//    let id: Int!
+//    let posterPath: String
+//    var trailerPath: String?
+//    let backdrop: String
+//    let title: String
+//    var releaseDate: Stringvar rating: Double
+//    let overview: String
+//    
+//    private enum CodingKey: String, CodingKey {
+//        case
+//        id,
+//        posterpath = "poster_path",
+//        trailerPath,
+//        backdrop = "backdrop_path",
+//        title,
+//        releaseDate = "release_date",
+//        rating = "vote_average",
+//        overview
+//    }
+//}
+//
+//struct APIResults: Decodable {
+//    let page: Int
+//    let numResults: Int
+//    let numPages: Int
+//    let movies: [Movie]
+//    
+//    private enum CodingKeys: String, CodingKey {
+//        case
+//            page,
+//            numResults = "total_results",
+//            numPages = "total_pages",
+//            movies = "results"
+//    }
+//}
+//
+//struct VideoResults: Decodable {
+//    let details: [VideoKey]
+//    
+//    private enum CodingKeys: String, CodingKey {
+//        case details: "results"
+//    }
+//}
+//
+//struct VideoKey: Decodable {
+//    let key: String
+//}
--- rush00/rush00/rush00/ViewController.swift
+++ rush00/rush00/rush00/ViewController.swift
@@ -8,6 +8,41 @@
 
 import UIKit
 
+struct Movie: Decodable {
+    let popularity: Double
+    var voteCount: Int
+    let video: Bool
+    let posterPath: String
+    let id: Int!
+    let adult: Bool
+    var backdropPath: String
+    var orginalLanguage: String
+    var orginalTitle: String
+    let genreIDs: [Int]
+    let title: String
+    var rating: Double
+    let overview: String
+    var releaseDate: String
+    
+    private enum codingKey: String, CodingKey {
+        case
+        popularity,
+        voteCount = "vote_count",
+        video,
+        posterPath = "poster_path",
+        id,
+        adult,
+        backdropPath = "backdrop_path",
+        originalLanguage = "original_language",
+        originaltitle = "original_title",
+        genreIDs,
+        title,
+        rating = "vote_average",
+        overview,
+        releaseDate = "release_date"
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -21,7 +56,7 @@ class ViewController: UIViewController {
     }
     
     func getData() {
-        guard let url = URL(string: "https://api.themoviedb.org/3/movie/550?api_key=a6df66754765aab6a38f08284dc49109") else { return }
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/top_rated?api_key=a6df66754765aab6a38f08284dc49109") else { return }
         
         let session = URLSession.shared
         session.dataTask(with: url) { (data, res, err) in
@@ -32,11 +67,13 @@ class ViewController: UIViewController {
             
             if let data = data {
                 do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    print("DATA: ")
-                    print(json)
+                    let decoder = JSONDecoder()
+//                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+//                    let jsonString = try! JSONSerialization.data(withJSONObject: json, options: [])
+                    let movies = try! decoder.decode(Movie.self, from: data)
+                    print(movies)
                 } catch {
-                    print(error)
+                    print(error.localizedDescription)
                 }
             }
         }.resume()
